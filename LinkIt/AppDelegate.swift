e8df1050c2588f4e5a2a795bc8646d9c50f21ed4
Dale Puckett <dale.puckett@att.net>
2016-11-27 21:11:21 -0600

    Initial Commit

---
+++ LinkIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkIt
+//
+//  Created by Dale Puckett on 11/27/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LinkIt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LinkIt
+//
+//  Created by Dale Puckett on 11/27/16.
+//  Copyright © 2016 k0hyd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
